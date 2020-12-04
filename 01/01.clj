59713d5d5ad22ef0c79e91dd743e8aff953bc155
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-12-04 16:28:44 +1100

    Improve advent of code 01

--- 01/01.clj
+++ 01/01.clj
@@ -1,12 +1,14 @@
 
-(defn read-input []
-    (with-open [rdr (clojure.java.io/reader "input.txt")]
-        (reduce conj [] (line-seq rdr))))
+(defn read-nums [filename]
+    (with-open [rdr (clojure.java.io/reader filename)]
+      (->> rdr
+       line-seq
+       (map #(Integer/parseInt %))
+       set)))
 
-(let [lines (read-input)]
-  (loop [line lines
-         cache #{}]
-    (let [val (Integer/parseInt (first line))]
-        (if (contains? cache val)
+(let [lines (read-nums "input.txt")]
+  (loop [line lines]
+    (let [val (first line)]
+        (if (contains? lines (- 2020 val))
             (* val (- 2020 val))
-            (recur (rest line) (conj cache (- 2020 val)))))))
+            (recur (rest line))))))
