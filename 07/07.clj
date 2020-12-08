f1aa87e4fe5bcfd8cec426af334fd0bf13b44924
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-12-08 14:29:35 +1100

    Day 7 P1

---
+++ 07/07.clj
@@ -0,0 +1,55 @@
+(ns luggage
+  (:require [clojure.string :as s]))
+(def line "light red bags contain 1 bright white bag, 2 muted yellow bags.")
+(def empty "faded blue bags contain no other bags.")
+
+
+(def data
+  (-> (slurp "input.txt")
+      (s/replace #"\sbags" "")
+      (s/replace #"\sbag" "")
+      (s/replace #"\d " "")
+      (s/split-lines)))
+
+data
+(defn add-entry
+  [dict line]
+  (let [[outer & inner] (s/split line #"(\.|, |\scontain\s)")]
+    (if (s/ends-with? line "no other.")
+      (assoc dict outer [])
+      (assoc dict outer inner))))
+
+
+(defn generate-map
+  [lines]
+  (loop [line lines
+         result {}]
+    (if (empty? line)
+      result
+      (recur (rest line) (add-entry result (first line))))))
+
+
+
+(defn contains-gold?
+  [bag bagMap]
+  (let [innerBags (get bagMap bag)]
+    (cond
+      (some #(= "shiny gold" %) innerBags) true
+      (some #(contains-gold? % bagMap) innerBags) true
+      :else false)))
+
+
+(def bagMap (generate-map data))
+
+(contains-gold? "shiny gold" bagMap)
+
+(def memo-contains-gold? (memoize contains-gold?))
+
+(defn solve-p1
+  []
+  (->> (map #(memo-contains-gold? % bagMap) (keys bagMap))
+       (filter identity)
+       count))
+
+
+(solve-p1)
