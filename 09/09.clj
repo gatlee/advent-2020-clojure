ca6c25821463c7879ee9b44717902872159722e9
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-12-13 14:42:54 +1100

    Day 09 Part 2

--- 09/09.clj
+++ 09/09.clj
@@ -23,7 +23,7 @@
 (def prev-length 25)
 
 
-(defn execute
+(defn get-invalid-num
   [nums]
   (loop [i prev-length]
     (let [curr-num (get nums i)
@@ -32,4 +32,28 @@
         (recur (inc i))
         curr-num))))
 
-(println (execute data))
+
+
+(def x [35 20 15 25 47 40 62 55 65 95 102 117 150 182 127 219 299 277 309 576])
+
+(def goal 127)
+
+(defn execute
+  [nums]
+  (let [goal (get-invalid-num nums)]
+    (println goal)
+    (loop [low 0
+           hi 1]
+      (let [run (subvec nums low hi)
+            total (apply + run)]
+        (println (str total " " low " " hi))
+        (cond
+          (= total goal) [(apply min run) (apply max run)]
+          (< total goal) (recur low (inc hi))
+          (> total goal) (recur (inc low) hi))))
+    ))
+
+
+
+(def answer
+  (apply + (execute data)))
